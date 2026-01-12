329cc385f566101887750135f0fe6b042e10ccd7
ISHANT-KB <ishantkrbag2007@gmail.com>
2026-01-12 16:05:12 +0530

    added avgerage and sum of array, integer check, matrix addition, structure demonstartion

---
+++ C/structure.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+struct Student {
+    char name[50];
+    int age;
+    float marks;
+};
+
+int main(){
+    struct Student s;
+    printf("enter name: ");
+    scanf("%s", s.name);
+    printf("enter age: ");
+    scanf("%d", &s.age);
+    printf("enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("Student Details:\n");
+    printf("Name: %s\n", s.name);
+    printf("Age: %d\n", s.age);
+    printf("Marks: %.2f\n", s.marks);
+    return 0;
+    
+}
